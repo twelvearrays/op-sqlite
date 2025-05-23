73a3a04075359df881ccdff9b21123941bd16a0c
Oscar Franco <ospfranco@gmail.com>
2025-05-23 11:02:34 +0200

    Working rn-web app

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,29 +1,94 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const appDirectory = path.resolve(__dirname);
+const {presets, plugins} = require(`${appDirectory}/babel.config.js`);
+const compileNodeModules = [
+  // Add every react-native package that needs compiling
+  // 'react-native-gesture-handler',
+].map(moduleName => path.resolve(appDirectory, `node_modules/${moduleName}`));
+
+const babelLoaderConfiguration = {
+  test: /\.(js|jsx|ts|tsx)$/, // Updated to include .jsx
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(__dirname, 'index.web.js'), // Entry to your application
+    path.resolve(__dirname, 'src'),
+    path.resolve(__dirname, 'component'),
+    ...compileNodeModules,
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      presets,
+      plugins,
+    },
+  },
+};
+
+const svgLoaderConfiguration = {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: '@svgr/webpack',
+    },
+  ],
+};
+
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  },
+};
+
+const tsLoaderConfiguration = {
+  test: /\.(ts)x?$/,
+  exclude: /node_modules|\.d\.ts$/, // this line as well
+  use: {
+    loader: 'ts-loader',
+    options: {
+      compilerOptions: {
+        noEmit: false, // this option will solve the issue
+      },
+    },
+  },
+};
 
 module.exports = {
-  entry: './index.web.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+  entry: {
+    app: path.join(__dirname, 'index.web.js'),
   },
-  mode: 'development',
+  output: {
+    path: path.resolve(appDirectory, 'dist'),
+    publicPath: '/',
+    filename: 'rnw.bundle.js',
+  },
   resolve: {
+    extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.js', '.js'],
     alias: {
       'react-native$': 'react-native-web',
     },
-    extensions: ['.web.js', '.js', '.jsx', '.ts', '.tsx'],
   },
   module: {
     rules: [
-      {test: /\.(js|ts)x?$/, use: 'babel-loader', exclude: /node_modules/},
-      {test: /\.wasm$/, type: 'webassembly/async'},
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      svgLoaderConfiguration,
+      tsLoaderConfiguration,
     ],
   },
-  experiments: {asyncWebAssembly: true},
-  devServer: {
-    static: './public',
-    hot: true,
-    historyApiFallback: true,
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(true),
+    }),
+  ],
 };
